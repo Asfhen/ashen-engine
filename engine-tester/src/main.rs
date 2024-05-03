6365bb11bbaa6f2fe341cb0817efdd3e0bd82ea5
João Pedro Lucchesi Roberto <joaopedro.lucchesi@codee.tech>
2024-05-03 16:42:50 -0300

    feat(windowing): added window handling class
    Added class for handling window management and user input

--- engine-tester/src/main.rs
+++ engine-tester/src/main.rs
@@ -1,2 +1,11 @@
+use ashen_engine::logger;
+use ashen_engine::window::Window;
 fn main() {
+    logger::init();
+
+    let mut window: Window = Window::new(1080, 720, "test");
+    window.init_gl();
+    while !window.should_close() {
+        window.update();
+    }
 }
---
+++ src/window.rs
@@ -0,0 +1,56 @@
+use std::sync::mpsc::Receiver;
+
+use glfw::{Context, Glfw, Key};
+
+pub struct Window {
+    glfw: glfw::Glfw,
+    window_handle: glfw::Window,
+    events: Receiver<(f64, glfw::WindowEvent)>
+}
+
+impl Window {
+    pub fn new(width: u32, height: u32, title: &str) -> Window {
+        let mut glfw: Glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+        let (mut window, events) = glfw
+            .create_window(width, height, title, glfw::WindowMode::Windowed)
+            .expect("Failed to create glfw window!");
+
+        window.set_framebuffer_size_polling(true);
+        window.set_key_polling(true);
+
+        Window {
+            glfw,
+            window_handle: window,
+            events,
+        }
+    }
+
+    pub fn init_gl(&mut self) {
+        self.window_handle.make_current();
+        gl::load_with(|s| self.window_handle.get_proc_address(s) as *const _);
+    }
+
+    pub fn should_close(&self) -> bool {
+        self.window_handle.should_close()
+    }
+
+    pub fn update(&mut self) {
+        self.process_events();
+        self.glfw.poll_events();
+        self.window_handle.swap_buffers();
+    }
+
+    fn process_events(&mut self) {
+        for (_, event) in glfw::flush_messages(&self.events) {
+            match event {
+                glfw::WindowEvent::FramebufferSize(width, height, ) => {
+                    unsafe { gl::Viewport(0, 0, width, height) }
+                }
+                glfw::WindowEvent::Key(Key::Escape, _, glfw::Action::Press, _) => {
+                    self.window_handle.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
+}
