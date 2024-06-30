554b85abd2c9030e23d61dfac8b119f1801577d8
Asfhen <joaopedro.sjc@gmail.com>
2024-06-30 14:09:53 -0300

    feat(window): add support for fullscreen application

--- src/window.rs
+++ src/window.rs
@@ -3,17 +3,36 @@ use std::sync::mpsc::Receiver;
 use glfw::{Context, Glfw, Key};
 
 pub struct Window {
-    glfw: glfw::Glfw,
+    pub glfw: glfw::Glfw,
     window_handle: glfw::Window,
     events: Receiver<(f64, glfw::WindowEvent)>,
 }
 
+pub enum WindowMode {
+    Windowed,
+    Fullscreen,
+    Borderless,
+}
+
 impl Window {
-    pub fn new(width: u32, height: u32, title: &str) -> Window {
+    pub fn new(width: u32, height: u32, title: &str, mode: WindowMode) -> Window {
         let mut glfw: Glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
         let (mut window, events) = glfw
-            .create_window(width, height, title, glfw::WindowMode::Windowed)
-            .expect("Failed to create glfw window!");
+            .with_primary_monitor(|glfw, monitors| match mode {
+                WindowMode::Windowed => {
+                    glfw.create_window(width, height, title, glfw::WindowMode::Windowed)
+                }
+                WindowMode::Fullscreen => glfw.create_window(
+                    width,
+                    height,
+                    title,
+                    monitors.map_or(glfw::WindowMode::Windowed, |m| {
+                        glfw::WindowMode::FullScreen(m)
+                    }),
+                ),
+                WindowMode::Borderless => todo!(),
+            })
+            .expect("Failed to create glfw window");
 
         window.set_framebuffer_size_polling(true);
         window.set_key_polling(true);
@@ -43,9 +62,9 @@ impl Window {
     fn process_events(&mut self) {
         for (_, event) in glfw::flush_messages(&self.events) {
             match event {
-                glfw::WindowEvent::FramebufferSize(width, height, ) => {
-                    unsafe { gl::Viewport(0, 0, width, height) }
-                }
+                glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                    gl::Viewport(0, 0, width, height)
+                },
                 glfw::WindowEvent::Key(Key::Escape, _, glfw::Action::Press, _) => {
                     self.window_handle.set_should_close(true)
                 }
